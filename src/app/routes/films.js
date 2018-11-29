1a1e04ac55f5a367c8f4f1bed9c01a2d7edad98e
Ignacio1224 <igaciocabrera1224@gmail.com>
2018-11-29 15:47:18 -0300

    Refactorización de código Terminado de Casi de LogIn, faltan detalles de JS como limpiar campos y el div de error

--- src/app/routes/films.js
+++ src/app/routes/films.js
@@ -1,45 +1,86 @@
 const dbConnection = require('../../config/dbConnection');
 
-let usernameLogged;
-let nametab = "Dashboard";
+const RenderPage = "Base/base";
+
+var usernameLogged;
+var LoggedUser = null;
 
 module.exports = app => {
     const connection = dbConnection();
 
+    // #################################### START ####################################
     app.get('/', (req, res) => {
         res.render("LogIn/LogIn", {
-            error : ""
+            error: ""
         });
     });
 
-    app.post('/changePassword', (req, res) => {
-        const {newPassword, verifiedPassword} = req.body;
-        
-        if (newPassword.length >= 8 && (newPassword === verifiedPassword)) {
-            const query = `UPDATE user SET passwords = "${newPassword}" WHERE userName = "${usernameLogged}"`
-            connection.query(query, (err, result) => {
-                res.render("Films/Films", {
-                    userNameLogged : usernameLogged
+    app.get('/LogIn', (req,res) => {
+        res.render("LogIn/LogIn", {
+            error:""
+        });
+    });
+
+    // #################################### LOGIN ####################################
+    // POST
+    app.post('/LogIn', (req, res) => {
+        const {
+            userName,
+            password
+        } = req.body;
+        const query = `SELECT * FROM user WHERE userName = "${userName}"`;
+
+        connection.query(query, (err, result) => {
+            if (result[0].userName === userName && result[0].passwords === password) {
+                res.render(RenderPage, {
+                    Page: {
+                        userNameLogged: result[0].userName,
+                        titleTab: "Dashboard"
+                    }
                 });
+            } else {
+                res.render("LogIn/LogIn", {
+                    error: "User or Password Incorrect"
+                });
+            }
+            // Set userLogged
+            LoggedUser = result[0].userName;
+        });
+    });
+
+
+    // #################################### ADD FILM ####################################
+    // GET
+    app.get('/AddFilm', (req, res) => {
+        if (LoggedUser != null) {
+            res.render(RenderPage, {
+                Page: {
+                    userNameLogged: LoggedUser,
+                    titleTab: "AddFilm"
+                }
+            });
+
+        } else {
+            res.render("LogIn/LogIn", {
+                error: "User not logged"
             });
         }
     });
 
-    app.post('/Films', (req, res) => {
-        const {userName, passwords} = req.body;
-        const query = `SELECT * FROM user WHERE userName = "${userName}"`;
-        
-        connection.query(query, (err, result) => {
-            if (result[0].userName === userName && result[0].passwords === passwords) {
-                res.render("Films/Films", {
-                    userNameLogged : result[0].userName,
-                    titleTab : "Dashboard"
-                });
-            } else {
-                res.render("LogIn/LogIn", {
-                    error : "User or Password Incorrect"
-                }); 
-            }
-        });
-    });
+    // app.post('/changePassword', (req, res) => {
+    //     const {
+    //         newPassword,
+    //         verifiedPassword
+    //     } = req.body;
+
+    //     if (newPassword.length >= 8 && (newPassword === verifiedPassword)) {
+    //         const query = `UPDATE user SET passwords = "${newPassword}" WHERE userName = "${usernameLogged}"`
+    //         connection.query(query, (err, result) => {
+    //             res.render("Films/Films", {
+    //                 userNameLogged: usernameLogged
+    //             });
+    //         });
+    //     }
+    // });
+
 }
