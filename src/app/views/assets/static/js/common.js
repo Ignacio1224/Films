c2dd3e7f7db57dd9fe211a8eb3579d3e7572da29
Ignacio1224 <igaciocabrera1224@gmail.com>
2018-11-30 09:01:51 -0300

    Terminado de navbar

--- src/app/views/assets/static/js/common.js
+++ src/app/views/assets/static/js/common.js
@@ -1,39 +1,7 @@
-/**
- * Created By:
- * Ignacio Cabrera
- */
+$(document).ready(function () {
 
-// Events
-$(document).ready(mostrarCuadroLogin);
-$('#modalSuccess').modal('hide');
-
-/**
- * Shows the LogIn square with an effect like fundition.
- */
-function mostrarCuadroLogin() {
-    $('#squareLogin').fadeIn('slow');
-}
-
-/**
- * Shows the div error change password.
- */
-$('#btnCambiarClave').click(verifyPass);
-function verifyPass() {
-    if (($('#txtPassword').val() !== $('#txtVerifiedPassword').val()) || $('#txtPassword').val().length < 8) {
-        $('#divErrorChangePassword').html('The new password is incorrect or both doesn`t coincide');
-        $('#divErrorChangePassword').css('display', 'block');
-        $('#modalChangePassword').modal('show');
-    } else {
-        alertOk("Password changed correctly");
-    }
-}
-
-/**
- * Shows the Success Modal.
- */
-function alertOk(msj) {
-    $('#modalSuccessTitulo').html(msj);
-    $('#modalSuccess').fadeTo(2000, 500).slideUp(500, function () {
-        $('#modalSuccess').slideUp(500);
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
     });
-}
+
+});
